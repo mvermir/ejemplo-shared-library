e0990515e516b0f217270b0cffcdfe4d89a33d02
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2020-12-18 12:34:24 -0300

    inicial

--- src/org/cl/Funciones.groovy
+++ src/org/cl/Funciones.groovy
@@ -9,8 +9,9 @@ def unirDosStrings(String param1, String param2){
 
 def mostrarNombre(){
 	def request = libraryResource 'org/cl/nombres.json'
+	def json    = readJSON text: request
 
-	println request
+	println json.name
 }
 
 return this;
