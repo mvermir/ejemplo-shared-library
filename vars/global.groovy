35f50e0e8226b6d7960e5e4cb2c762b9ca4315ea
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2020-12-18 12:25:21 -0300

    inicial

--- vars/global.groovy
+++ vars/global.groovy
@@ -12,21 +12,21 @@ def call(String param1, String param2){
             stage('Pipeline'){
                 steps{
                     script{
+
+                        def funciones   = new Funciones()
+
                         stage('Inicio'){
                             println 'Inicio'
                             println 'String 1: ' + param1
                             println 'String 2: ' + param2
                         }
 
-                        stage('UnirDosStrings'){
-                            try {
-                                def funciones   = new Funciones()
-                                def nuevoString = funciones.unirDosStrings(param1, param2)
-                                println 'Union de 2 Strings: ' + nuevoString            
-                            }
-                            catch(Exception e) {
-                                error ('Se ha capturado el siguiente error: ' + e)
-                            }
+                        stage('Union'){
+                            println 'Union de 2 Strings: ' + funciones.unirDosStrings(param1, param2)          
+                        }
+
+                        stage('MostrarNombre'){
+                            println 'Nombre obtenido desde Json: ' + funciones.mostrarNombre()
                         }
                     }
                 }
