b82f7e71930cd456f8ea08a2218d21c23b99720a
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2020-12-18 12:08:38 -0300

    inicial

---
+++ src/org/cl/Funciones.groovy
@@ -0,0 +1,25 @@
+//Funciones.groovy
+
+//Classpath
+package org.cl
+
+
+//Script
+
+def unirDosStrings(String param1, String param2){
+	return param1 + param2
+}
+
+return this;
+
+/* Clase
+
+class Funciones {
+
+	def implementacion(String param1, String param2){
+		return param1 + param2
+	}
+
+	//Escribir código con notación parecida a Java.
+}
+*/
---
+++ vars/global.groovy
@@ -0,0 +1,28 @@
+//global.groovy
+
+import org.cl.*
+
+def call(String param1, String param2){
+
+	println 'Ejecución de Pipeline'
+
+	stage('Inicio'){
+		println 'Inicio'
+		println 'String 1: ' + param1
+		println 'String 2: ' + param2
+	}
+
+	stage('UnirDosStrings'){
+		try {
+			def funciones   = new Funciones()
+			def nuevoString = funciones.unirDosStrings(param1, param2)
+			println 'Union de 2 Strings: ' + nuevoString			
+		}
+		catch(Exception e) {
+			error ('Se ha capturado el siguiente error: ' + e)
+		}
+	}
+
+}
+
+return this;
