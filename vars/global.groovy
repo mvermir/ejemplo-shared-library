80dc07a2729ea56d40fdd7845d14b8f2d7933a60
Rodrigo Osias Pino Galleguillos <rodrigo.pino@bci.cl>
2020-12-18 12:14:39 -0300

    inicial

--- vars/global.groovy
+++ vars/global.groovy
@@ -4,25 +4,34 @@ import org.cl.*
 
 def call(String param1, String param2){
 
-	println 'Ejecución de Pipeline'
+    println 'Ejecución de Pipeline'
 
-	stage('Inicio'){
-		println 'Inicio'
-		println 'String 1: ' + param1
-		println 'String 2: ' + param2
-	}
+    pipeline{
+        stages{
+            stage('Pipeline'){
+                steps{
+                    script{
+                        stage('Inicio'){
+                            println 'Inicio'
+                            println 'String 1: ' + param1
+                            println 'String 2: ' + param2
+                        }
 
-	stage('UnirDosStrings'){
-		try {
-			def funciones   = new Funciones()
-			def nuevoString = funciones.unirDosStrings(param1, param2)
-			println 'Union de 2 Strings: ' + nuevoString			
-		}
-		catch(Exception e) {
-			error ('Se ha capturado el siguiente error: ' + e)
-		}
-	}
-
+                        stage('UnirDosStrings'){
+                            try {
+                                def funciones   = new Funciones()
+                                def nuevoString = funciones.unirDosStrings(param1, param2)
+                                println 'Union de 2 Strings: ' + nuevoString            
+                            }
+                            catch(Exception e) {
+                                error ('Se ha capturado el siguiente error: ' + e)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 return this;
